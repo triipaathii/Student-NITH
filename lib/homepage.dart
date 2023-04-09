9b6b13c210c9b8e49ec6d73167ec37538fcfb1ad
Tejas <tejasprashar9@gmail.com>
2023-04-09 23:07:00 +0530

    poda myrae

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:student_nith/hostel.dart';
 import 'package:student_nith/profile.dart';
+import 'package:student_nith/result.dart';
+import 'package:student_nith/semreg1.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -39,53 +42,70 @@ class Homepage extends StatelessWidget {
             image: DecorationImage(
                 image: AssetImage("assets/images/bg.jpg"),
                 fit: BoxFit.cover,
-                opacity: 0.7)),
+                opacity: 0.8)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color(0xff640f12),
-                  borderRadius: BorderRadius.circular(16)),
-              height: height * 0.15,
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(horizontal: width * 0.25),
-              child: Text(
-                "SEMESTER REGISTRATION",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color(0xff640f12),
-                  borderRadius: BorderRadius.circular(16)),
-              height: height * 0.15,
-              width: width * 0.5,
-              child: Text(
-                "HOSTEL ROOM ALLOTMENT",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color(0xff640f12),
-                  borderRadius: BorderRadius.circular(16)),
-              height: height * 0.15,
-              width: width * 0.5,
-              child: Text(
-                "RESULT",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
-              ),
-            )
+            InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff640f12),
+                      borderRadius: BorderRadius.circular(16)),
+                  height: height * 0.15,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: width * 0.25),
+                  child: Text(
+                    "SEMESTER REGISTRATION",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Semreg1()));
+                }),
+            InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff640f12),
+                      borderRadius: BorderRadius.circular(16)),
+                  height: height * 0.15,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: width * 0.25),
+                  child: Text(
+                    "HOSTEL ROOM ALLOTMENT",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Hostel()));
+                }),
+            InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff640f12),
+                      borderRadius: BorderRadius.circular(16)),
+                  height: height * 0.15,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: width * 0.25),
+                  child: Text(
+                    "RESULT",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.abel(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Result()));
+                }),
           ],
         ),
       ),
